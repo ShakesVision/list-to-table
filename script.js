f85b21bef305e3c21ab038cd20f080a61bd0828b
Shakeeb Ahmad <shakeeb.ahmad@simplifiedautomation.ca>
2022-03-05 09:00:31 +0530

    highlight related changes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let n = document.querySelector('.column');
 let txt = document.querySelector('.text');
 let output = document.querySelector('.output');
+let selectedItems = [];
 function generate() {
     let items = txt.value.split('\n');
     let c = n.value;
     let tData = "";
-    tData += `<table>`
+    tData += `<table class='data-table rtl'>`
     for (let i = 0; i < items.length; i++) {
         if (c == n.value || c === 0) {
             tData += `<tr>`
         }
-        tData += `<td> <span>${i+1}</span> ${items[i]} </td>`
+        tData += `<td id="${i + 1}" onclick="highlightTd(${i + 1})"> <span>${i + 1}</span> ${items[i]} </td>`
         if (c == 1) {
             tData += `</tr>`
         }
@@ -18,12 +19,31 @@ function generate() {
         if (c == 0)
             c = n.value;
     };
-    console.log(tData);
     output.innerHTML += `${tData}</table>`
+}
+
+function highlightTd(id) {
+    document.getElementById(id).classList.toggle('active');
+    selectedItems.push(item);
+    // document.querySelector('.selected .selected-count').innerText = selectedItems.length;
+}
+
+function selectAll() {
+    document.querySelectorAll('table td').forEach(t => t.classList.add('active'));
+}
+function deselectAll() {
+    document.querySelectorAll('table td').forEach(t => t.classList.remove('active'));
+}
+function invertSelection() {
+    document.querySelectorAll('table td').forEach(t => t.classList.toggle('active'));
 }
 
 function clearFields() {
     output.innerHTML = n.value = txt.value = '';
+}
+
+function toggleTableDirection() {    
+    document.querySelectorAll('.data-table').forEach(t => t.classList.toggle('rtl'));
 }
 
 const copyToClipboard = (text) => navigator.clipboard?.writeText && navigator.clipboard.writeText(text);
